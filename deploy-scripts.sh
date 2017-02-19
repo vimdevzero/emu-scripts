70136104c518334d958cefaf68d85b37a6151b98
vimdevzero <vimdevzero@gmail.com>
2017-02-19 15:41:57 +0000

    added mednafen config deployment and snes controls for ps3

--- deploy-scripts.sh
+++ deploy-scripts.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+if [ -z "$1" ] ; then
+  echo "ERROR - no username supplied"
+  exit
+fi 
+
 mkdir /home/$1/bin
 
 cp ./bin/* /home/$1/bin
 
 chmod +x /home/$1/bin/*
+
+cp conf/mednafen-09x.cfg /home/$1/.mednafen/mednafen-09x.cfg
